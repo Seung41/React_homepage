af98986bf8d20b38d65c6db6d3c6fcbe9d411ed4
Seung41 <seung9441@naver.com>
2022-12-07 16:15:38 +0900

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,19 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+
+export const Banner = () => {
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to my Portfolio</span>
+                        <h1>{"Hi I'm webdecoded"}<span className="wrap">web developer</span></h1>
+                        <p>aaaaaaaaaa</p>
+                        <button onClick={() => console.log('connect')}>Let's connect</button>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
